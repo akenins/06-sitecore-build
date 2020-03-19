179d7adcb8ce2e75b53961b7b4958af208be2f38
Aleks Kenins <akenins@hitachi-solutions.com>
2020-03-19 10:53:21 -0400

    Deferred sticky nav load to after document is loaded

--- src/scripts/source/sticky-header.js
+++ src/scripts/source/sticky-header.js
@@ -1,14 +1,18 @@
-let header = document.getElementById('global-header')
-let headroomOptions = {
-  offset: 0,
-  tolerance: 10,
-  classes: {
-    initial: 'headroom animated',
-    pinned: 'headroom--pinned',
-    unpinned: 'headroom--unpinned',
-  },
+function stickyHeader() {
+  let header = document.getElementById('global-header')
+  let headroomOptions = {
+    offset: 0,
+    tolerance: 10,
+    classes: {
+      initial: 'headroom animated',
+      pinned: 'headroom--pinned',
+      unpinned: 'headroom--unpinned',
+    },
+  }
+
+  let headroom = new Headroom(header, headroomOptions)
+
+  headroom.init()
 }
 
-let headroom = new Headroom(header, headroomOptions)
-
-headroom.init()
+document.addEventListener('DOMContentLoaded', stickyHeader)
