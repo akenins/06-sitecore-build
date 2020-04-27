96838382d90418134877a3fb47f3d6c482874e74
Aleks Kenins <alekskenins@gmail.com>
2020-04-27 12:09:26 -0400

    Added Resource Center Filter functionality.

---
+++ src/scripts/source/resource-gallery-filter-controller.js
@@ -0,0 +1,39 @@
+$(function () {
+  const $resourceType = $('#resource-type')
+  const $resourceIndustry = $('#resource-industry')
+  const $resourceSolutionArea = $('#resource-solution-area')
+
+  const $resourceFilters = [
+    $resourceType,
+    $resourceIndustry,
+    $resourceSolutionArea,
+  ]
+
+  let url = new URL(document.location) // Gets current URL
+  let params = new URLSearchParams(url.search) // Extracts all parameters from url string
+  let tags = params.getAll('tag') // Creates an array of all "tag" parameters
+
+  if (tags.length !== 0) {
+    $('#clear-filters').append(
+      `<a href="/resources-gallery" class="chip">Clear Filters</a>`
+    )
+  }
+
+  tags.map((tag) => {
+    $('#active-filters').append(`<span class="chip filter">${tag}</span>`)
+  })
+
+  $resourceFilters.forEach((filter) => {
+    filter.on('change', function () {
+      params.set('tag', $(this).val())
+
+      if (tags.length === 0) {
+        window.location = `${url}?${params}`
+      } else if ($(this).val() === 'default') {
+        return false
+      } else {
+        window.location = `${url}&${params}`
+      }
+    })
+  })
+})
--- src/scripts/source/resource-gallery-tab-controller.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  const urlParameter = location.href.split('?tag=')[1]
-  $(
-    `.filter-column.filter-tab a[href^="/resources-gallery?tag=${urlParameter}"]`
-  ).addClass('active')
-})
