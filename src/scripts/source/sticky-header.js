5eeff9fed9acb3beae3931e718756727e2e7f44f
Aleks Kenins <alekskenins@gmail.com>
2020-05-05 11:18:55 -0400

    Updated sticky-nav

--- src/scripts/source/sticky-header.js
+++ src/scripts/source/sticky-header.js
@@ -1,17 +1,18 @@
-let header = document.getElementById('global-header')
-
-function initHeadroom() {
-  headroom = new Headroom(header, {
-    offset: 50,
+function stickyHeader() {
+  let header = document.getElementById('global-header')
+  let headroomOptions = {
+    offset: 0,
     tolerance: 10,
     classes: {
       initial: 'headroom animated',
       pinned: 'headroom--pinned',
       unpinned: 'headroom--unpinned',
     },
-  })
+  }
+
+  let headroom = new Headroom(header, headroomOptions)
 
   headroom.init()
 }
 
-document.addEventListener('DOMContentLoaded', initHeadroom)
+document.addEventListener('DOMContentLoaded', stickyHeader)
