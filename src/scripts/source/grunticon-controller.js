0864ec39c365ce0c9a3e3044818b0bd7b23f47ac
Aleks Kenins <alekskenins@gmail.com>
2020-05-07 14:07:34 -0400

    Implemented gulpicon functionality.

---
+++ src/scripts/source/grunticon-controller.js
@@ -0,0 +1,17 @@
+// let grunticonPath = 'http://localhost:3000/assets/icons/',
+//   svgPath,
+//   pngPath,
+//   fallbackPath
+
+// function doGrunticon() {
+//   svgPath = grunticonPath + 'icons.data.svg.css'
+//   pngPath = grunticonPath + 'icons.data.png.css'
+//   fallbackPath = grunticonPath + 'icons.fallback.css'
+//   console.log(svgPath)
+// }
+
+// grunticon([svgPath, pngPath, fallbackPath], function () {
+//   grunticon.svgLoadedCORSCallback()
+// })
+
+// document.addEventListener('DOMContentLoaded', doGrunticon)
