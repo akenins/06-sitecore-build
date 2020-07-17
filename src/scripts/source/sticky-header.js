e076573f22a29ec3c74b454070b19100bdb0f974
Aleks Kenins <alekskenins@gmail.com>
2020-07-16 22:01:19 -0400

    Added new icon for alert bar.

--- src/scripts/source/sticky-header.js
+++ src/scripts/source/sticky-header.js
@@ -1,18 +1,48 @@
 function stickyHeader() {
-  let header = document.getElementById('global-header')
-  let headroomOptions = {
-    offset: 0,
-    tolerance: 10,
-    classes: {
-      initial: 'headroom animated',
-      pinned: 'headroom--pinned',
-      unpinned: 'headroom--unpinned',
-    },
-  }
+  let header = document.getElementById('global-header'),
+    headroomOptions = {
+      offset: 0,
+      tolerance: 10,
+      classes: {
+        initial: 'headroom animated',
+        pinned: 'headroom--pinned',
+        unpinned: 'headroom--unpinned',
+      },
+    }
 
   let headroom = new Headroom(header, headroomOptions)
 
   headroom.init()
+
+  // // Alert Bar
+  // let alertBar = $('#alert-bar')
+
+  // // Hide alert bar if cookie is set.
+  // if (Cookies.set('hitachi-alert')) {
+  //   alertBar.hide()
+  // } else {
+  //   alertBar.show()
+  // }
+
+  // $('#alert-bar .alert-close').on('click', function (e) {
+  //   if (e.currentTarget.tagName === 'a') {
+  //     e.preventDefault()
+  //   }
+  //   $('#alert-bar').slideUp()
+  //   let alertCookie = Cookies.get('hitachi-alert')
+  //   if (alertCookie === undefined) {
+  //     // Set cookie so we don't see the alert on every page load
+  //     Cookies.set('hitachi-alert', '1')
+  //     setTimeout(function () {
+  //       alertBar.addClass('disabled')
+  //     }, 500)
+  //   }
+  //   return false
+  // })
+
+  // function resetHeaderHeight() {}
+
+  // $(window).on('resize', resetHeaderHeight)
 }
 
 document.addEventListener('DOMContentLoaded', stickyHeader)
