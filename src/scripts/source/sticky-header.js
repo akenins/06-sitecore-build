0ebae23748c1db86f7f9fea20412bb2f68aa69dc
Aleks Kenins <alekskenins@gmail.com>
2020-07-21 17:22:33 -0400

    Updates to alert bar.

--- src/scripts/source/sticky-header.js
+++ src/scripts/source/sticky-header.js
@@ -1,5 +1,6 @@
 function stickyHeader() {
   let header = document.getElementById('global-header'),
+    initialHeight = header.offsetHeight,
     headroomOptions = {
       offset: 0,
       tolerance: 10,
@@ -14,35 +15,32 @@ function stickyHeader() {
 
   headroom.init()
 
-  // // Alert Bar
-  // let alertBar = $('#alert-bar')
+  // Alert Bar
+  let alertBar = $('#alert-bar')
 
-  // // Hide alert bar if cookie is set.
-  // if (Cookies.set('hitachi-alert')) {
-  //   alertBar.hide()
-  // } else {
-  //   alertBar.show()
-  // }
+  //Hide alert bar if cookie is set.
+  if (Cookies.set('hitachi-alert')) {
+    alertBar.hide()
+    alertBar.addClass('disabled')
+  } else {
+    alertBar.show()
+  }
 
-  // $('#alert-bar .alert-close').on('click', function (e) {
-  //   if (e.currentTarget.tagName === 'a') {
-  //     e.preventDefault()
-  //   }
-  //   $('#alert-bar').slideUp()
-  //   let alertCookie = Cookies.get('hitachi-alert')
-  //   if (alertCookie === undefined) {
-  //     // Set cookie so we don't see the alert on every page load
-  //     Cookies.set('hitachi-alert', '1')
-  //     setTimeout(function () {
-  //       alertBar.addClass('disabled')
-  //     }, 500)
-  //   }
-  //   return false
-  // })
-
-  // function resetHeaderHeight() {}
-
-  // $(window).on('resize', resetHeaderHeight)
+  $('#alert-bar .alert-close').on('click', function (e) {
+    if (e.currentTarget.tagName === 'a') {
+      e.preventDefault()
+    }
+    $('#alert-bar').slideUp()
+    let alertCookie = Cookies.get('hitachi-alert')
+    if (alertCookie === undefined) {
+      // Set cookie so we don't see the alert on every page load
+      Cookies.set('hitachi-alert', '1')
+      setTimeout(function () {
+        alertBar.addClass('disabled')
+      }, 500)
+    }
+    return false
+  })
 }
 
 document.addEventListener('DOMContentLoaded', stickyHeader)
