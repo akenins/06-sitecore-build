0e30172245b9c60e483a706b0c146ce7305df3cf
Aleks Kenins <alekskenins@gmail.com>
2020-07-27 11:34:21 -0400

    Updated alert bar and hero margin when ber is active.

--- src/scripts/source/alert-bar.js
+++ src/scripts/source/alert-bar.js
@@ -2,22 +2,26 @@ let alertBar = $('#alert-bar'),
   alertBarCloseButton = $('#alert-bar .alert-close'),
   alertCookie = Cookies.get('hitachi-alert'),
   header = document.getElementById('global-header'),
+  hero = $('.hero .container .heading-container'),
   initialHeight = header.offsetHeight
 
 function onDocumentReady() {
   //Hide alert bar if cookie is set.
   $(header).addClass('alert')
-  //   if (Cookies.set('hitachi-alert')) {
-  //     alertBar.hide()
-  //   } else {
-  //     alertBar.show()
-  //   }
-  //   resetHeaderHeight()
+  if (Cookies.set('hitachi-alert')) {
+    alertBar.hide()
+  } else {
+    alertBar.show()
+    hero.addClass('alert')
+  }
+  resetHeaderHeight()
+  console.log(hero)
 }
 
 function closeAlertBar() {
   alertBar.slideUp()
   $(header).removeClass('alert')
+  hero.removeClass('alert')
 }
 
 function setAlertBarDisabledCookie() {
